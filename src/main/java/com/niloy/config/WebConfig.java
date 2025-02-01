65818ecd345770142cd612488f40edb5d32cb570
niloychandrasarker <niloysarker.cs@gmail.com>
2025-02-02 02:22:43 +0600

    update cors config-

--- src/main/java/com/niloy/config/WebConfig.java
+++ src/main/java/com/niloy/config/WebConfig.java
@@ -13,8 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")  // Apply CORS to all endpoints
                 .allowedOrigins("*")  // Specify allowed origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
-                .allowedHeaders("*")  // Allow all headers
-                .allowCredentials(true)  // Allow credentials (cookies, authorization headers)
-                .maxAge(3600);  // Cache pre-flight response for 1 hour
+                .allowedHeaders("*");  // Allow all headers
     }
 }
